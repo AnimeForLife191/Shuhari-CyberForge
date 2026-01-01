f4740da38fa2cab59e1fb2e24d14a291246a0f92
AnimeForLife191 <190670013+AnimeForLife191@users.noreply.github.com>
2026-01-01 10:37:44 -0700

    Added WMI helper functions and refactored antivirus scanner to use them

--- tools/warden/src/lib.rs
+++ tools/warden/src/lib.rs
@@ -2,6 +2,8 @@ pub mod antivirus;
 pub mod updates;
 pub mod firewall;
 
+mod common;
+
 pub use antivirus::scanner::antivirus_wmi_api;
 pub use updates::scanner::update_com_api;
 pub use firewall::scanner::firewall_com_api;
