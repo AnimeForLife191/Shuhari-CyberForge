67b857c248cb2548101f7302456e315cf5043ec2
AnimeForLife191 <190670013+AnimeForLife191@users.noreply.github.com>
2026-01-05 12:44:20 -0700

    Updated display logic for Antivirus Module

--- tools/warden/src/antivirus/display.rs
+++ tools/warden/src/antivirus/display.rs
@@ -6,59 +6,61 @@ pub fn display_antivirus(products: &[ProductInfo], verbose: bool) {
     println!("ANTIVIRUS PROTECTION AUDIT"); // Title for display
     println!("{}", "=".repeat(30));
 
+    if verbose {display_scan_details();}
+
     if products.is_empty() {
         println!("No Antivirus Products Found");
         return;
     }
 
-    // If verbose is NOT used
-    if !verbose {
-        println!("Summary:");
-        println!(" - Products Found: {}", products.len()); // We use the length of the vec to get our count
-        let active_count = products.iter().filter(|prod| prod.is_active).count(); // Filtering out the none active products
-        println!(" - Active: {}/{}", active_count, products.len()); // Verifying how many products are active
-        let real_time_count = products.iter().filter(|prod| prod.is_realtime).count(); // Filtering out real-time protection
-        println!(" - Real-time Protection: {}/{}", real_time_count, products.len()); // Verifying how many products have real-time on
-        let definitions_count = products.iter().filter(|prod| prod.definitions_new).count(); // Filtering out non updated definitions
-        println!(" - Definitions Updated: {}/{}\n", definitions_count, products.len()); // Verifying how many products definitions are current
+    display_summary(products);
 
-        println!("Product Details:\n"); // For loop to iterate through products
-        for (i, prod) in products.iter().enumerate() {
-            println!("{}. {}", i + 1, prod.name); // Name of product
-            println!(" - Status: {}", if prod.is_active {"Active"} else {"Inactive"}); // Product is Active or Inactive
-            println!(" - Real-time: {}", if prod.is_realtime {"Enabled"} else {"Disabled"}); // Real-time On or Off
-            println!(" - Definitions: {}\n", if prod.definitions_new {"Up-to-date"} else {"Out-of-date"}); // Diffinitions are up-to-date
-        }
-    } else { 
-        // Verbose Scan
-        println!("Scan Details:"); 
-        println!(" - Scan Started: {:02}:{:02}:{:02} UTC", get_time().0, get_time().1, get_time().2); // NOTE: 0 is hours, 1 is minutes, 2 is seconds
-        println!(" - WMI Namespace: ROOT\\SecurityCenter2"); // Namespaced used
-        println!(" - Query: Select displayName, productState FROM AntiVirusProduct\n"); // Query used
+    product_details(products, verbose);
 
-        println!("Summary:");
-        println!(" - Products Found: {}", products.len());
-        let active_count = products.iter().filter(|prod| prod.is_active).count();
-        println!(" - Active: {}/{}", active_count, products.len());
-        let real_time_count = products.iter().filter(|prod| prod.is_realtime).count();
-        println!(" - Real-time Protection: {}/{}", real_time_count, products.len());
-        let definitions_count = products.iter().filter(|prod| prod.definitions_new).count();
-        println!(" - Definitions Updated: {}/{}\n", definitions_count, products.len());
+    if verbose {display_technical();}
+}
 
-        println!("Product Details:\n");
-        for (i, prod) in products.iter().enumerate() {
-            println!("{}. {}", i + 1, prod.name);
-            println!(" - Status: {}", if prod.is_active {"Active"} else {"Inactive"});
-            println!(" - Real-time: {}", if prod.is_realtime {"Enabled"} else {"Disabled"});
-            println!(" - Definitions: {}", if prod.definitions_new {"Up-to-date"} else {"Out-of-date"});
+fn display_scan_details() {
+    let (h, m, s) = get_time();
+
+    println!("Scan Details:"); 
+    println!(" - Scan Started: {:02}:{:02}:{:02} UTC", h, m, s);
+    println!(" - WMI Namespace: ROOT\\SecurityCenter2");
+    println!(" - Query: Select displayName, productState FROM AntiVirusProduct");
+    println!();
+}
+
+fn display_summary(products: &[ProductInfo]) {
+    println!("Summary:");
+    println!(" - Products Found: {}", products.len());
+    let active_count = products.iter().filter(|prod| prod.is_active).count();
+    println!(" - Active: {}/{}", active_count, products.len());
+    let real_time_count = products.iter().filter(|prod| prod.is_realtime).count();
+    println!(" - Real-time Protection: {}/{}", real_time_count, products.len());
+    let definitions_count = products.iter().filter(|prod| prod.definitions_new).count();
+    println!(" - Definitions Updated: {}/{}", definitions_count, products.len());
+    println!();
+}
+
+fn product_details(products: &[ProductInfo], verbose: bool) {
+    println!("Product Details:"); // For loop to iterate through products
+    println!();
+    for (i, prod) in products.iter().enumerate() {
+        println!("{}. {}", i + 1, prod.name); // Name of product
+        println!(" - Status: {}", if prod.is_active {"Active"} else {"Inactive"}); // Product is Active or Inactive
+        println!(" - Real-time: {}", if prod.is_realtime {"Enabled"} else {"Disabled"}); // Real-time On or Off
+        println!(" - Definitions: {}", if prod.definitions_new {"Up-to-date"} else {"Out-of-date"}); // Definitions are up-to-date
+        if verbose {
             println!(" - Product Hexadecimal State: 0x{:X}", prod.state); // Hexadecimal of product
             println!(" - Product Raw State: {}", prod.state); // Raw state of product
-            println!();
         }
-
-        println!("Technical Information:");
-        println!(" - COM Apartment: MTA (Multi-threaded)");
-        println!(" - WMI Context: CLSCTX_INPROC_SERVER");
         println!();
     }
+}
+
+fn display_technical() {
+    println!("Technical Information:");
+    println!(" - COM Apartment: MTA (Multi-threaded)");
+    println!(" - WMI Context: CLSCTX_INPROC_SERVER");
+    println!();
 }
